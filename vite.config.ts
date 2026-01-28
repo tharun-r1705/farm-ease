b038e990c2b97641cf25bb7d14649eb395c270fe
Tharun R <tharunr1705@gmail.com>
2026-01-28 16:04:21 +0530

    Deployment fix v2

--- vite.config.ts
+++ vite.config.ts
@@ -21,8 +21,6 @@ export default defineConfig({
       output: {
         manualChunks: {
           'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          'map-vendor': ['leaflet', 'react-leaflet'],
-          'chart-vendor': ['recharts'],
         },
       },
     },
